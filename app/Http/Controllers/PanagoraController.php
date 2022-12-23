76fa3323f4efcaa758b645163468d5e8d71f1b50
Pedro Liima <pedro.henrique@pede.ai>
2022-12-22 23:48:15 -0300

    Finaliza configuração do plugin que gera PDF e inicia a implementacao para gerar os documentos de todos os votantes do evento apenas dos selecionados

--- app/Http/Controllers/PanagoraController.php
+++ app/Http/Controllers/PanagoraController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Promise;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class PanagoraController extends Controller
 {
@@ -24,11 +26,11 @@ class PanagoraController extends Controller
         );
 
         $http_status_code = $response->getStatusCode();
-        $response = json_decode($response->getBody()->getContents());
+        $vote_people = json_decode($response->getBody()->getContents());
 
         $message = 'Lista de votantes obtida com sucesso';
 
-        // O retorno 404 esperado segundo a documentação não está funcionando, caso um evento não existá a api retorna um erro 500
+        // O retorno 404 esperado segundo a documentação não está funcionando, caso um evento não exista a api retorna um erro 500
         switch ($http_status_code) {
             case 401:
                 $message = 'Token inválido!';
@@ -39,13 +41,49 @@ class PanagoraController extends Controller
                 break;
         }
 
-        if ($request->getAllVoters == 'true') {
-            dd('k');
+        foreach ($vote_people as $vote_person) {
+            $vote_ids[] = $vote_person->id;
+        }
+
+        if ($request->getAllDocuments == 'true') {
+            $this->votePersonPDF($vote_ids, $event_code);
+
+            die();
         }
 
         return response([
             'message' => $message,
-            'response' => $response
+            'response' => $vote_people
         ], $http_status_code);
+    }
+
+    public function votePersonPDF(Array $vote_ids, $event_code)
+    {
+        $panagora_api = new GuzzleClient();
+
+        foreach ($vote_ids as $vote_id) {
+            $response[] = $panagora_api->requestAsync(
+                'GET',
+                env('PANAGORA_API_ENDPOINT') . "/eventos/$event_code/votante/$vote_id",
+                [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . env('PANAGORA_API_TOKEN'),
+                        'Content-Type' => 'application/json',
+                    ],
+                    'http_errors' => false
+                ]
+            );
+
+            break;
+        }
+
+        $responses = Promise\Utils::unwrap($response);
+
+        foreach ($responses as $response) {
+            $voters = json_decode($response->getBody()->getContents());
+
+            $pdf = Pdf::loadView('pdf.voter', ['name' => $voters->nome]);
+            dd($pdf->download('voter.pdf'));
+        }
     }
 }
